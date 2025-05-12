c9b6ccc8e21d6ea0270efbb33e615afca0def738
NP305 <npala305@gmail.com>
2025-05-12 13:52:38 -0400

    Update log_feedback.py

--- log_feedback.py
+++ log_feedback.py
@@ -7,7 +7,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
-db = client["peroxide_logs"]
+db = client["chatbotlogs"]
 collection = db["feedbacks"]
 
 def log_to_mongo(soru, ajan, yanit, geri_bildirim=None, yorum=""):
@@ -20,3 +20,7 @@ def log_to_mongo(soru, ajan, yanit, geri_bildirim=None, yorum=""):
         "yorum": yorum
     }
     collection.insert_one(entry)
+
+
+def get_logs_from_mongo():
+    return list(collection.find().sort("timestamp", -1))
